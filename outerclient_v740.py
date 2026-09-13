97b1d41f6bdf30a28cf446628fd7f57ef6975860
Zallevvz <zallevv20@gmail.com>
2026-09-13 16:53:05 +0200

    Add OuterClient 7.4.0 entrypoint

---
+++ outerclient_v740.py
@@ -0,0 +1,17 @@
+"""OuterClient 7.4.0 packaged entry point."""
+import outerclient as _outerclient
+from outerclient_v730_patch import apply_v730
+from outerclient_v731_patch import install as apply_v731
+from outerclient_v732_patch import install as apply_v732
+from outerclient_v740_motion import install as apply_v740
+
+apply_v730(_outerclient)
+apply_v731(_outerclient)
+apply_v732(_outerclient)
+apply_v740(_outerclient)
+
+APP_VERSION = _outerclient.APP_VERSION
+OuterClient = _outerclient.OuterClient
+
+if __name__ == "__main__":
+    OuterClient().mainloop()
